0980a0261b6f66c684c9037e3b6e611499806c4c
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-24 03:21:37 +0530

    Added Chat login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const connect = require('./config/database-config')
+const Chat = require('./models/chat')
 
 const { PORT } = require('./config/server-config')
 
@@ -9,16 +11,43 @@ const server = http.createServer(app);
 const io = socketio(server)
 
 io.on('connection', (socket) => {
-    console.log('User has connected', socket.id);
+    socket.on('join_room', (data) => {
+        socket.join(data.roomId, function () {
+            console.log('Joining the Room')
+        } )
+    })
 
-    socket.on('text_send', (data) => {
+    socket.on('text_sent', async (data) => {
         console.log(data)
-        socket.broadcast.emit('text_receive', data)
+        const chat = await Chat.create({
+            roomId: data.roomId,
+            user: data.username,
+            content: data.msg
+        })
+        io.to(data.roomId).emit('text_received', data)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.to(data.roomId).emit('someone_typing')
     })
   });
 
+app.get('/chat/:roomId', async (req, res) => {
+    const chats = await Chat.find({
+        roomId: req.params.roomId
+    }).select('content user')
+    res.render('index', {
+        name: 'Ravi',
+        id: req.params.roomId,
+        chats: chats
+    })
+})
+
+app.set('view engine', 'ejs')
 app.use('/', express.static(__dirname + '/public'))
 
 server.listen(PORT, async () => {
     console.log(`Server started at port ${PORT}`)
+    await connect();
+    console.log('MongoDB connected')
 })
--- src/public/script.js
+++ src/public/script.js
@@ -5,12 +5,12 @@ let inputMsg = document.getElementById('newtext')
 let textList = document.getElementById('textList')
 
 btn.onclick = function exec() {
-    socket.emit('text_send', {
+    socket.emit('text_sent', {
         msg: inputMsg.value
     })
 }
 
-socket.on('text_receive', (data) => {
+socket.on('text_received', (data) => {
     let litext = document.createElement('li')
     litext.innerText = data.msg;
     textList.appendChild(litext)
